d1d6d495bc247eed2e9e3a9be9218e1b6b282797
Vicky-tseng <o30329o@gmail.com>
2024-01-19 22:42:44 +0800

    Add files via upload

---
+++ Controllers/VIckyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vicky.Controllers
+{
+    public class VIckyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
